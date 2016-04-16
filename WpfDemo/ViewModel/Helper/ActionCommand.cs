f46061192501126922aafdbc96bba63047d99d5f
Johnny P <johnnnyppp@gmail.com>
2016-04-16 15:19:55 +0200

    Implemented IsEnabled propfull

--- WpfDemo/ViewModel/Helper/ActionCommand.cs
+++ WpfDemo/ViewModel/Helper/ActionCommand.cs
@@ -9,12 +9,40 @@ namespace WpfDemo.ViewModel.Helper
 {
     class ActionCommand : ICommand
     {
+        #region Fields
+
         private Action _action;
+
+        #endregion
+
+        #region Constructors
 
         public ActionCommand(Action command)
         {
             _action = command;
+        } 
+        
+        # endregion
+
+        #region Properties
+
+        private bool _isEnabled = true;
+
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                _isEnabled = value;
+
+                if(CanExecuteChanged != null)
+                {
+                    CanExecuteChanged(this, EventArgs.Empty);
+                }
+            }
         }
+
+        #endregion
 
         #region ICommand implementation
 
