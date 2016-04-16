856f272a9bccaaf0d4352b2733c663ff950a9a4d
Johnny P <johnnnyppp@gmail.com>
2016-04-16 14:47:52 +0200

    Implemented ActionCommand : ICommand

--- WpfDemo/ViewModel/Helper/ActionCommand.cs
+++ WpfDemo/ViewModel/Helper/ActionCommand.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace WpfDemo.ViewModel.Helper
 {
-    class ActionCommand
+    class ActionCommand : ICommand
     {
+        #region ICommand implementation
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
